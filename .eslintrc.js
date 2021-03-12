2ea79d30b4268d113c3b5c008dd6e526aa27ec5e
smallbee3 <smallbee3@gmail.com>
2021-03-12 11:16:03 +0900

    Fix the eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,75 @@
 module.exports = {
-  root: true,
-  extends: '@dooboo/eslint-config',
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // eslint-plugin-prettier + eslint-config-prettier
+    'plugin:react/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    warnOnUnsupportedTypeScriptVersion: true,
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'simple-import-sort', // eslint-plugin-simple-import-sort
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
+    semi: [2, 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'arrow-parens': ['error', 'always'],
+    'no-new-object': 'error',
+    'no-array-constructor': 'error',
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    'no-useless-escape': 'warn',
+    'lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true },
+    ],
+    'padding-line-between-statements': [
+      'warn',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: ['block-like'],
+      },
+    ],
+    // eslint
     'eslint-comments/no-unlimited-disable': 0,
     'eslint-comments/no-unused-disable': 0,
+    // third-party
+    'simple-import-sort/imports': 'error',
+    // typescript
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/no-var-requires': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    // react
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
 };
